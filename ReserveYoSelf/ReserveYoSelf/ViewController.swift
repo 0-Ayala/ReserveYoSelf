011eb81abd5f26b5aee6a491b6b9ac8a38eaac77
artur oganezov <artur@arturs-MacBook-Pro.local>
2018-03-11 16:14:31 -0500

    readme updates

---
+++ ReserveYoSelf/ReserveYoSelf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReserveYoSelf
+//
+//  Created by artur oganezov on 3/11/18.
+//  Copyright © 2018 iOSfinal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
