b1a38cb4797f3c5393b4ba56678eb3e83c42f0a3
Olivia Ayala <olivia.ayala96@gmail.com>
2018-03-12 19:02:02 -0500

    Added table buttons and a view controller for the table nums view.

--- ReserveYoSelf/ReserveYoSelf/ViewController.swift
+++ ReserveYoSelf/ReserveYoSelf/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func switchBack(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
